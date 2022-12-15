ef2021d6842ad70e4b432911b08ac6a5c078c569
Everton de Oliveira <evertonoliveira1029@gmail.com>
2022-12-14 22:08:54 -0300

    adiciona modelo de dados

--- penalty_calculate/penalty_calculate.py
+++ penalty_calculate/penalty_calculate.py
@@ -5,7 +5,8 @@ class PenaltyCalculate:
 
     def __init__(self, csv_file):
         self._csv_file = csv_file
-    
+
     def csv_reader(self):
-        csv_file = FileParser(self._csv_file).take_national_identity_cards_with_license_plate()
-        return csv_file
+        return FileParser(
+            self._csv_file
+        ).take_id_cards_with_license_plate()
--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -8,17 +8,17 @@ class TestIntegration(unittest.TestCase):
     def test_penalty_calculate(self):
         penalty_calculate = PenaltyCalculate("Transit Ticket input 1.csv")
 
-        national_identity_cards_with_license_plate = penalty_calculate.csv_reader()
+        national_id_cards_with_license_plate = penalty_calculate.csv_reader()
 
         self.assertEqual(
-            national_identity_cards_with_license_plate, 
-            [
-                ["467191153", "Josevaldo Cal. O. Teiro"], 
+            national_id_cards_with_license_plate, [
                 ["467191153", "Josevaldo Cal. O. Teiro"],
                 ["467191153", "Josevaldo Cal. O. Teiro"],
-                ["467191153", "Josevaldo Cal. O. Teiro"], 
-                ["276787067", "Osvaldo Plinio"], 
+                ["467191153", "Josevaldo Cal. O. Teiro"],
+                ["467191153", "Josevaldo Cal. O. Teiro"],
+                ["276787067", "Osvaldo Plinio"],
                 ["149178360", "Gerusa Juventina"],
-                ["ARE-9420", "KVI-2310", "KVI-2310", "ARE-9420", "BIO-9626", "SOS-3257"]
+                ["ARE-9420", "KVI-2310", "KVI-2310",
+                    "ARE-9420", "BIO-9626", "SOS-3257"]
             ]
         )
