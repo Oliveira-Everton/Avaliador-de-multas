fad9c762d428eeae1bb76a3f9967d414505f0096
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-01-06 22:55:26 -0300

    Exclui a classe TrafficViolations

--- penalty_calculate/models/traffic_violations.py
+++
@@ -1,8 +0,0 @@
-class TrafficViolations:
-
-    def __init__(self, traffic_violations=[]):
-        self._traffic_violations = traffic_violations
-
-    @property
-    def traffic_violations(self):
-        return self._traffic_violations
--- test/unit/parsers/test_file_parser.py
+++ test/unit/parsers/test_file_parser.py
@@ -8,15 +8,8 @@ class TestFileParser(unittest.TestCase):
     def test_output_file(self):
         csv_parser = FileParser("Transit Ticket input 1.csv")
 
-        output_file = csv_parser.output_file()
+        traffic_violations = csv_parser.traffic_violations()
 
         self.assertEqual(
-            output_file, [
-                '467191153; Josevaldo Cal. O. Teiro; ARE-9420',
-                '467191153; Josevaldo Cal. O. Teiro; KVI-2310',
-                '467191153; Josevaldo Cal. O. Teiro; KVI-2310',
-                '467191153; Josevaldo Cal. O. Teiro; ARE-9420',
-                '276787067; Osvaldo Plinio; BIO-9626',
-                '149178360; Gerusa Juventina; SOS-3257'
-            ]
+            len(traffic_violations), 6
         )
