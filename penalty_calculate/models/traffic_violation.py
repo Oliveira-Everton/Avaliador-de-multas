1a16fb4256f536a7109c0a337ed1ebcd1bb0f474
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-01-10 19:23:25 -0300

    Implementa o método __eq__ no TrafficViolation para comparar objetos

--- penalty_calculate/models/traffic_violation.py
+++ penalty_calculate/models/traffic_violation.py
@@ -1,7 +1,3 @@
-from penalty_calculate.models.identity_card import IdentityCard
-from penalty_calculate.models.license_plate import LicensePlate
-
-
 class TrafficViolation:
 
     def __init__(self, identity_card, license_plate):
@@ -15,3 +11,10 @@ class TrafficViolation:
     @property
     def license_plate(self):
         return self._license_plate
+
+    def __eq__(self, other):
+        identity_cards_equals = \
+            self._identity_card.number == other._identity_card.number
+        license_plates_equals = \
+            self._license_plate.number == other._license_plate.number
+        return identity_cards_equals == license_plates_equals
