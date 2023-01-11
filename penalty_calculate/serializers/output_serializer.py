dddefbb191bea8d4166afd6a1c6c66ad20253c7c
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-01-11 20:06:17 -0300

    Organiza os métodos contrutores, agora eles apenas atribuem seus parâmetros

--- penalty_calculate/serializers/output_serializer.py
+++ penalty_calculate/serializers/output_serializer.py
@@ -2,11 +2,11 @@ class OutputSerializer:
     _OFFENDER_DATA = '{0}; {1}; {2}'
 
     def __init__(self, traffic_violations):
-        self._output_identity_cards(traffic_violations)
+        self._traffic_violations = traffic_violations
 
-    def _output_identity_cards(self, traffic_violations):
+    def _output(self):
         offender_data = []
-        for violation in traffic_violations:
+        for violation in self._traffic_violations:
             offender_data.append(
                 self._OFFENDER_DATA.format(
                     violation.identity_card.number,
@@ -14,7 +14,7 @@ class OutputSerializer:
                     violation.license_plate.number
                 )
             )
-        self._offender_data = offender_data
+        return offender_data
 
     def output_string(self):
-        return self._offender_data
+        return self._output()
