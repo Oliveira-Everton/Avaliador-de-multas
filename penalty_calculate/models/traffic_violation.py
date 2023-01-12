110b02d92414e892bf0f5a4e79217455eda6d0d3
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-01-12 18:32:28 -0300

    Altera o retorno do método __eq__ do TrafficViolation

--- penalty_calculate/models/traffic_violation.py
+++ penalty_calculate/models/traffic_violation.py
@@ -12,8 +12,7 @@ class TrafficViolation:
         return self._license_plate
 
     def __eq__(self, other):
-        identity_cards_equals = \
-            self._identity_card.number == other._identity_card.number
-        license_plates_equals = \
-            self._license_plate.number == other._license_plate.number
-        return identity_cards_equals == license_plates_equals
+        return (
+            self.identity_card.number == other.identity_card.number,
+            self.license_plate.number == other.license_plate.number
+        )
