1cf07c65d7ba04578a2805892b13ac59ab9b3c0c
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-01-20 18:02:51 -0300

    Altera o retorno da propriedade identity_card do TrafficViolation

--- penalty_calculate/serializers/output_serializer.py
+++ penalty_calculate/serializers/output_serializer.py
@@ -1,5 +1,7 @@
 class OutputSerializer:
     _OFFENDER_DATA = '{0}; {1}; {2}'
+    _NUMBER = 0
+    _NAME = 1
 
     def __init__(self, traffic_violations):
         self._traffic_violations = traffic_violations
@@ -7,23 +9,14 @@ class OutputSerializer:
     def _output(self):
         offender_data = []
         for violation in self._traffic_violations:
-            self._violation = violation
             offender_data.append(
                 self._OFFENDER_DATA.format(
-                    self._identity_card.number,
-                    self._identity_card.name,
-                    self._license_plate.number
+                    violation.identity_card[self._NUMBER],
+                    violation.identity_card[self._NAME],
+                    violation.license_plate
                 )
             )
         return offender_data
 
     def output_string(self):
         return self._output()
-
-    @property
-    def _identity_card(self):
-        return self._violation.identity_card
-
-    @property
-    def _license_plate(self):
-        return self._violation.license_plate
