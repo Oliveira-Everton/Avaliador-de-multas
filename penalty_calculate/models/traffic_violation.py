2a6c3b85dd93f8c845502497af8ec66689827eff
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-01-27 17:16:14 -0300

    Padroniza as propriedades do TrafficViolation

--- penalty_calculate/models/traffic_violation.py
+++ penalty_calculate/models/traffic_violation.py
@@ -1,22 +1,23 @@
 class TrafficViolation:
     def __init__(self, identity_card, license_plate):
-        self._identity_card = identity_card
-        self._license_plate = license_plate
+        self._identity_card_name = identity_card.name
+        self._identity_card_number = identity_card.number
+        self._license_plate_number = license_plate.number
 
     @property
     def identity_card_number(self):
-        return self._identity_card.number
+        return self._identity_card_number
 
     @property
     def identity_card_name(self):
-        return self._identity_card.name
+        return self._identity_card_name
 
     @property
-    def license_plate(self):
-        return self._license_plate.number
+    def license_plate_number(self):
+        return self._license_plate_number
 
     def __eq__(self, other):
         return (
             self.identity_card_number == other.identity_card_number,
-            self.license_plate == other.license_plate
+            self.license_plate_number == other.license_plate_number
         )
