5001e2139e1780d378775bd8106f2d876f5804c6
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-02-01 14:56:47 -0300

    Padroniza os nomes dos testes

--- test/unit/models/test_identity_card.py
+++ test/unit/models/test_identity_card.py
@@ -4,14 +4,14 @@ from penalty_calculate.models.identity_card import IdentityCard
 
 
 class TestIdentityCard(unittest.TestCase):
-    def test_property_identity_name(self):
+    def test_identity_card_name(self):
         identity_card = IdentityCard("37.594.403-5", "Dtcv. Olivera")
 
         identity_name = identity_card.name
 
         self.assertEqual(identity_name, "Dtcv. Olivera")
 
-    def test_property_identity_number(self):
+    def test_identity_number(self):
         identity_card = IdentityCard("37.594.403-5", "Dtcv. Olivera")
 
         identity_number = identity_card.number
--- test/unit/models/test_license_plate.py
+++ test/unit/models/test_license_plate.py
@@ -4,7 +4,7 @@ from penalty_calculate.models.license_plate import LicensePlate
 
 
 class TestLicensePlate(unittest.TestCase):
-    def test_property_license_plate(self):
+    def test_license_plate_number(self):
         license_plate = LicensePlate("QBJ-6840")
 
         plate_number = license_plate.number
