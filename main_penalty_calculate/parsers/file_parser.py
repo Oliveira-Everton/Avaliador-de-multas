f5f87ecdd996bcb9d0f3255e165e9c36bae53ca5
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-04-18 15:13:08 -0300

    Adiciona o Unicode UTF-8 ao 'FileParser'

--- main_penalty_calculate/parsers/file_parser.py
+++ main_penalty_calculate/parsers/file_parser.py
@@ -14,12 +14,17 @@ class FileParser:
     _FIRST_LINE = 0
     _READ_PARAMETER = 'r'
     _DELIMITER = ';'
+    _UNICODE = 'utf-8'
 
     def __init__(self, file_name):
         self._file_name = file_name
 
     def _convert_file(self):
-        with open(self._file_name, mode=self._READ_PARAMETER) as file:
+        with open(
+            self._file_name,
+            mode=self._READ_PARAMETER,
+            encoding=self._UNICODE
+        ) as file:
             reader_csv = csv.reader(file, delimiter=self._DELIMITER)
             return list(enumerate(reader_csv))
 
