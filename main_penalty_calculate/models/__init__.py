380a6002704385f5acabacc60103419b7a9955dc
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-04-19 18:42:30 -0300

    Revert "Adiciona o modelo de dados 'ViolatorAvaliation'"
    This reverts commit 4720183a5ee94b15112c2543e90912bb6b3042e2.

--- main_penalty_calculate/models/__init__.py
+++ main_penalty_calculate/models/__init__.py
@@ -1,4 +1,3 @@
 from .identity_card import IdentityCard
 from .license_plate import LicensePlate
 from .traffic_violation import TrafficViolation
-from .violator_avaliation import ViolatorAvaliation
