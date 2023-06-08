3b60349b7bcf235cf26bf556604df49cdfac54f6
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-06-07 21:16:39 -0300

    Quebra 'agroup_violators_avaliations' em funções menores

--- main_penalty_calculate/builders/violators_avaliations_builder.py
+++ main_penalty_calculate/builders/violators_avaliations_builder.py
@@ -6,42 +6,66 @@ from ..models import (
 
 class ViolatorsAvaliationsBuilder:
     _LICENSE_PLATE_IN_LIST = 0
+    _FIRST_TRAFFIC_VIOLATION = 0
 
     def __init__(self, traffic_violations):
         self._traffic_violations = traffic_violations
-        self._violators_avaliations = []
 
-    def _agroup_violators_avaliations(self):
-        revised_violators = []
-        for violator in self._violators_avaliations:
-            uniq = True
-            for revised_violator in revised_violators:
-                if violator.identity_card_number == \
-                        revised_violator.identity_card_number:
-                    uniq = False
-                    if violator.license_plate_numbers[
-                        self._LICENSE_PLATE_IN_LIST
-                    ] not in revised_violator.license_plate_numbers:
-                        revised_violator.license_plate_numbers.append(
-                            violator.license_plate_numbers[
-                                self._LICENSE_PLATE_IN_LIST
-                            ]
-                        )
-            if uniq:
-                revised_violators.append(violator)
-        return revised_violators
+    def _search_identity_card_number(
+        self,
+        id_card_number,
+        violators_list
+    ):
+        for violators in violators_list:
+            if violators.identity_card_number == id_card_number:
+                return True
+        return False
+
+    def _search_license_plate_number(
+        self,
+        license_plate_number,
+        violators_list
+    ):
+        for violators in violators_list:
+            if violators == license_plate_number:
+                return True
+        return False
+
+    def _agroup_license_plates(
+        self,
+        license_plate_to_be_checked,
+        violators_list
+    ):
+        for violator in violators_list:
+            if not self._search_license_plate_number(
+                license_plate_to_be_checked,
+                violator.license_plate_numbers
+            ):
+                violator.license_plate_numbers.append(
+                    license_plate_to_be_checked
+                )
 
     def build_violators_avaliations(self):
+        violators_avaliations = []
         for traffic_violation in self._traffic_violations:
-            self._violators_avaliations.append(
-                ViolatorAvaliation(
-                    identity_card=IdentityCard(
-                        number=traffic_violation.identity_card_number,
-                        name=traffic_violation.identity_card_name
-                    ),
-                    license_plates=[
-                        traffic_violation.license_plate_number
-                    ]
+            if self._search_identity_card_number(
+                traffic_violation.identity_card_number,
+                violators_avaliations
+            ):
+                self._agroup_license_plates(
+                    traffic_violation.license_plate_number,
+                    violators_avaliations
                 )
-            )
-        return self._agroup_violators_avaliations()
+            else:
+                violators_avaliations.append(
+                    ViolatorAvaliation(
+                        identity_card=IdentityCard(
+                            number=traffic_violation.identity_card_number,
+                            name=traffic_violation.identity_card_name
+                        ),
+                        license_plates=[
+                            traffic_violation.license_plate_number
+                        ]
+                    )
+                )
+        return violators_avaliations
