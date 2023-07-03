cbc024a709d0b937ab656d76ecf8fa0b8e16925c
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-07-03 19:58:13 -0300

    Add novo cenário de teste o 'TestIntegration'.

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -18,3 +18,18 @@ class TestIntegration(unittest.TestCase):
                 '149178360; Gerusa Juventina; SOS-3257; 4'
             ]
         )
+
+    def test_evaluate_infractors_with_invalid_demerit_points(self):
+        penalty_calculate = PenaltyCalculate(
+            'Transit Ticket input 2.csv'
+        )
+
+        violators_avaliations = penalty_calculate.evaluate_infractors()
+
+        self.assertEqual(
+            violators_avaliations, [
+                '375944035; Dtcv. Olivera; QBJ-6840; 0',
+                '475936607; Weihi Imawee; NEK-6986; 0',
+                '138469945; Morgan Lyori; MGN-9130; 0'
+            ]
+        )
