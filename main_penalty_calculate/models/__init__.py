168449c0363447073be8327131ebc665cf85ac4c
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-07-20 20:37:34 -0300

    Remove classe 'TypeInfraction'

--- main_penalty_calculate/models/__init__.py
+++ main_penalty_calculate/models/__init__.py
@@ -2,4 +2,3 @@ from .identity_card import IdentityCard
 from .license_plate import LicensePlate
 from .traffic_violation import TrafficViolation
 from .violator_avaliation import ViolatorAvaliation
-from .type_infraction import TypeInfraction
