b804fffab808e8898d8ee1a8c8d7359fb58a91b7
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-07-24 16:24:56 -0300

    Adiciona novos testes ao 'TestTrafficViolationBuilder'

--- test/unit/builders/test_traffic_violation_builder.py
+++ test/unit/builders/test_traffic_violation_builder.py
@@ -3,6 +3,7 @@ import unittest
 from datetime import datetime
 
 from main_penalty_calculate.builders import TrafficViolationBuilder
+from main_penalty_calculate.constants import PenaltyStrings
 from main_penalty_calculate.models import (
     TrafficViolation,
     IdentityCard,
@@ -11,8 +12,7 @@ from main_penalty_calculate.models import (
 
 
 class TestTrafficViolationBuilder(unittest.TestCase):
-    def test_build_traffic_violations(self):
-
+    def test_build_traffic_violations_type_infraction_very_serious(self):
         converted_file = [
             (
                 0, [
@@ -52,7 +52,160 @@ class TestTrafficViolationBuilder(unittest.TestCase):
                     license_plate=LicensePlate(
                         number='MGN-9130'
                     ),
-                    type_infraction='very_serious',
+                    type_infraction=PenaltyStrings.VERY_SERIOUS,
+                    infraction_date=datetime.fromisoformat(
+                        '1275-01-30 15:00:00'
+                    ),
+                    notification_date=datetime.fromisoformat(
+                        '1275-02-01 10:00:00'
+                    )
+                )
+            ]
+        )
+
+    def test_build_traffic_violations_type_infraction_serious(self):
+        converted_file = [
+            (
+                0, [
+                    'Placa',
+                    'Tipo de infração',
+                    'Data da infração',
+                    'Data da notificação',
+                    'Data de pagamento',
+                    'RG do infrator',
+                    'Nome do infrator',
+                ]
+            ),
+            (
+                1, [
+                    'MGN-9130',
+                    'Grave',
+                    '1275-01-30 15:00:00',
+                    '1275-02-01 10:00:00',
+                    '',
+                    '138469945',
+                    'Morgan'
+                ]
+            )
+        ]
+
+        traffic_violations = TrafficViolationBuilder(
+            converted_file
+        ).build_traffic_violations()
+
+        self.assertEqual(
+            traffic_violations, [
+                TrafficViolation(
+                    identity_card=IdentityCard(
+                        name='Morgan',
+                        number='138469945'
+                    ),
+                    license_plate=LicensePlate(
+                        number='MGN-9130'
+                    ),
+                    type_infraction=PenaltyStrings.SERIOUS,
+                    infraction_date=datetime.fromisoformat(
+                        '1275-01-30 15:00:00'
+                    ),
+                    notification_date=datetime.fromisoformat(
+                        '1275-02-01 10:00:00'
+                    )
+                )
+            ]
+        )
+
+    def test_build_traffic_violations_type_infraction_average(self):
+        converted_file = [
+            (
+                0, [
+                    'Placa',
+                    'Tipo de infração',
+                    'Data da infração',
+                    'Data da notificação',
+                    'Data de pagamento',
+                    'RG do infrator',
+                    'Nome do infrator',
+                ]
+            ),
+            (
+                1, [
+                    'MGN-9130',
+                    'Média',
+                    '1275-01-30 15:00:00',
+                    '1275-02-01 10:00:00',
+                    '',
+                    '138469945',
+                    'Morgan'
+                ]
+            )
+        ]
+
+        traffic_violations = TrafficViolationBuilder(
+            converted_file
+        ).build_traffic_violations()
+
+        self.assertEqual(
+            traffic_violations, [
+                TrafficViolation(
+                    identity_card=IdentityCard(
+                        name='Morgan',
+                        number='138469945'
+                    ),
+                    license_plate=LicensePlate(
+                        number='MGN-9130'
+                    ),
+                    type_infraction=PenaltyStrings.AVERAGE,
+                    infraction_date=datetime.fromisoformat(
+                        '1275-01-30 15:00:00'
+                    ),
+                    notification_date=datetime.fromisoformat(
+                        '1275-02-01 10:00:00'
+                    )
+                )
+            ]
+        )
+
+    def test_build_traffic_violations_type_infraction_light(self):
+        converted_file = [
+            (
+                0, [
+                    'Placa',
+                    'Tipo de infração',
+                    'Data da infração',
+                    'Data da notificação',
+                    'Data de pagamento',
+                    'RG do infrator',
+                    'Nome do infrator',
+                ]
+            ),
+            (
+                1, [
+                    'MGN-9130',
+                    'Leve',
+                    '1275-01-30 15:00:00',
+                    '1275-02-01 10:00:00',
+                    '',
+                    '138469945',
+                    'Morgan'
+                ]
+            )
+        ]
+
+        traffic_violations = TrafficViolationBuilder(
+            converted_file
+        ).build_traffic_violations()
+
+        self.assertEqual(
+            traffic_violations, [
+                TrafficViolation(
+                    identity_card=IdentityCard(
+                        name='Morgan',
+                        number='138469945'
+                    ),
+                    license_plate=LicensePlate(
+                        number='MGN-9130'
+                    ),
+                    type_infraction=PenaltyStrings.LIGHT,
                     infraction_date=datetime.fromisoformat(
                         '1275-01-30 15:00:00'
                     ),
