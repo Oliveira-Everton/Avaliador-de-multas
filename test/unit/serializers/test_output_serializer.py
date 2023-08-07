41dd87ae4bad61e280ccf28eb64a50ce7884dfa3
Everton de Oliveira <evertonoliveira1029@gmail.com>
2023-08-07 19:51:51 -0300

    Adiciona novo cenário de teste ao 'TestOutputSerializer'

--- test/unit/serializers/test_output_serializer.py
+++ test/unit/serializers/test_output_serializer.py
@@ -6,18 +6,17 @@ from main_penalty_calculate.serializers import OutputSerializer
 
 class TestOutputSerializer(unittest.TestCase):
     def test_output_string(self):
-        first_violator_avaliation = ViolatorAvaliation(
-            identity_card=IdentityCard('29.441.369-8', 'Aoki'),
-            license_plates=['UCH-6237'],
-            demerit_points=4
-        )
-        second_violator_avaliation = ViolatorAvaliation(
-            identity_card=IdentityCard('19.632.142-6', 'Takashi'),
-            license_plates=['IDE-3516'],
-            demerit_points=7
-        )
         violators_avaliations = [
-            first_violator_avaliation, second_violator_avaliation
+            ViolatorAvaliation(
+                identity_card=IdentityCard('29.441.369-8', 'Aoki'),
+                license_plates=['UCH-6237'],
+                demerit_points=4
+            ),
+            ViolatorAvaliation(
+                identity_card=IdentityCard('19.632.142-6', 'Takashi'),
+                license_plates=['IDE-3516'],
+                demerit_points=7
+            )
         ]
 
         output_string = OutputSerializer(violators_avaliations).output_string()
@@ -26,5 +25,24 @@ class TestOutputSerializer(unittest.TestCase):
             output_string, [
                 '29.441.369-8; Aoki; UCH-6237; 4',
                 '19.632.142-6; Takashi; IDE-3516; 7'
+            ]
+        )
+
+    def test_output_string_with_more_license_plates_in_same_identity_card(
+        self
+    ):
+        violators_avaliations = [
+            ViolatorAvaliation(
+                identity_card=IdentityCard('29.441.369-8', 'Aoki'),
+                license_plates=['UCH-6237', 'HUG-2023'],
+                demerit_points=7
+            )
+        ]
+
+        output_string = OutputSerializer(violators_avaliations).output_string()
+
+        self.assertEqual(
+            output_string, [
+                '29.441.369-8; Aoki; UCH-6237, HUG-2023; 7'
             ]
         )
